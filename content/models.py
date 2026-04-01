dc91849dcdb20c1afe9c48013b71e9bee616e49c
joyjce <joycelucenaacerden24@gmail.com>
2026-04-01 23:53:13 +0800

    adding comments

--- content/models.py
+++ content/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+ # UserProfile model to extend the built-in User model with a premium status
+ 
 class UserProfile(models.Model):
     # Connects to the standard Django User
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
